355224afa2f96eb1ee5f9081f508dcd951b4cc43
ChengPoSheng <bencer@outlook.com>
2023-06-10 21:14:09 +0800

    serve file

--- myApp.js
+++ myApp.js
@@ -1,7 +1,10 @@
 let express = require('express');
 let app = express();
+
+absolutePath = __dirname + "/views/index.html";
+
 app.get("/", function(req, res) {
-    res.send('Hello Express');
+    res.sendFile(absolutePath);
 });
 
 
